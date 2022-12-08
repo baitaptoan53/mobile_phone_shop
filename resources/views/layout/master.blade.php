cd3b66003e9da65b35739dcb2c89a608c7ac5fdd
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2022-12-08 18:04:16 +0700

    template

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+                   <meta charset="UTF-8">
+                   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                   <title>Mobile Phone Shop</title>
+                   {{-- style --}}
+                   <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}" type="text/css">
+                   <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}" type="text/css">
+
+                   {{-- style --}}
+                   <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
+                       <!-- Tweaks for older IEs-->
+                   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+                   {{-- reponsive --}}
+                   <link rel="stylesheet" href="{{asset('css/responsive.css')}}" type="text/css">
+                   {{-- fontawesome --}}
+                   <link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}" type="text/css">
+                   {{-- bootstrap --}}
+                   <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" type="text/css">
+                   {{-- favicon --}}
+                   <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" type="image/x-icon">
+                   {{-- Scrollbar Custom CSS --}}
+                   <link rel="stylesheet" href="{{asset('css/jquery.mCustomScrollbar.min.css')}}" type="text/css">
+                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+
+
+
+</head>
+<body class="main-layout ">
+                   <!-- header -->
+                  @include('layout.header')
+                   <!-- end header -->
+                   @include('layout.slider')
+               
+                   <!-- about -->
+                   <div class="about">
+                       <div class="container">
+                           <div class="row">
+                               <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
+                                   <div class="about_img">
+                                       <figure><img src="images/about.png" alt="img" /></figure>
+                                   </div>
+                               </div>
+                               <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
+                                   <div class="about_box">
+                                       <h3>About Us</h3>
+                                       <span>Our Mobile Shop</span>
+                                       <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the </p>
+               
+                                   </div>
+                              
+                           </div>
+                       </div>
+                   </div>
+                   </div>
+                   <!-- end about -->
+               
+                   <!-- brand -->
+                   <div class="brand">
+                       <div class="container">
+                           <div class="row">
+                               <div class="col-md-12">
+                                   <div class="titlepage">
+                                       <h2>Our Brand</h2>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="brand-bg">
+                           <div class="container">
+                               <div class="row">
+                                   <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
+                                       <div class="brand_box">
+                                           <img src="images/1.png" alt="img" />
+                                           <h3>$<strong class="red">100</strong></h3>
+                                           <span>Mobile Phone</span>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                       </div>
+                                   </div>
+                                   <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
+                                       <div class="brand_box">
+                                           <img src="images/2.png" alt="img" />
+                                           <h3>$<strong class="red">100</strong></h3>
+                                           <span>Mobile Phone</span>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                       </div>
+                                   </div>
+                                   <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
+                                       <div class="brand_box">
+                                           <img src="images/3.png" alt="img" />
+                                           <h3>$<strong class="red">100</strong></h3>
+                                           <span>Mobile Phone</span>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                       </div>
+                                   </div>
+                                   <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                                       <div class="brand_box">
+                                           <img src="images/4.png" alt="img" />
+                                           <h3>$<strong class="red">100</strong></h3>
+                                           <span>Mobile Phone</span>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                       </div>
+                                   </div>
+                                   <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 mrgn">
+                                       <div class="brand_box">
+                                           <img src="images/5.png" alt="img" />
+                                           <h3>$<strong class="red">100</strong></h3>
+                                           <span>Mobile Phone</span>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                       </div>
+                                   </div>
+                                   <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 mrgn">
+                                       <div class="brand_box">
+                                           <img src="images/6.png" alt="img" />
+                                           <h3>$<strong class="red">100</strong></h3>
+                                           <span>Mobile Phone</span>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                           <i><img src="images/star.png"/></i>
+                                       </div>
+                                   </div>
+                                   <div class="col-md-12">
+                                       <a class="read-more">See More</a>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               
+                   <!-- end brand -->
+                   <!-- clients -->
+                   <div class="clients">
+                       <div class="container">
+                           <div class="row">
+                               <div class="col-md-12">
+                                   <div class="titlepage">
+                                       <h2>what say our clients</h2>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="clients_red">
+                       <div class="container">
+                           <div id="testimonial_slider" class="carousel slide" data-ride="carousel">
+                               <!-- Indicators -->
+                               <ul class="carousel-indicators">
+                                   <li data-target="#testimonial_slider" data-slide-to="0" class=""></li>
+                                   <li data-target="#testimonial_slider" data-slide-to="1" class="active"></li>
+                                   <li data-target="#testimonial_slider" data-slide-to="2" class=""></li>
+                               </ul>
+                               <!-- The slideshow -->
+                               <div class="carousel-inner">
+                                   <div class="carousel-item">
+                                       <div class="testomonial_section">
+                                           <div class="full center">
+                                           </div>
+                                           <div class="full testimonial_cont text_align_center cross_layout">
+                                               <div class="cross_inner">
+                                                   <h3>Due markes<br><strong class="ornage_color">Rental</strong></h3>
+                                                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i>
+                                                   </p>
+                                                   <div class="full text_align_center margin_top_30">
+                                                       <img src="icon/testimonial_qoute.png">
+                                                   </div>
+                                               </div>
+                                           </div>
+                                       </div>
+                                   </div>
+               
+                                   <div class="carousel-item active">
+               
+                                       <div class="testomonial_section">
+                                           <div class="full center">
+                                           </div>
+                                           <div class="full testimonial_cont text_align_center cross_layout">
+                                               <div class="cross_inner">
+                                                   <h3>Due markes<br><strong class="ornage_color">Rental</strong></h3>
+                                                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i>
+                                                   </p>
+                                                   <div class="full text_align_center margin_top_30">
+                                                       <img src="icon/testimonial_qoute.png">
+                                                   </div>
+                                               </div>
+                                           </div>
+                                       </div>
+               
+                                   </div>
+               
+                                   <div class="carousel-item">
+               
+                                       <div class="testomonial_section">
+                                           <div class="full center">
+                                           </div>
+                                           <div class="full testimonial_cont text_align_center cross_layout">
+                                               <div class="cross_inner">
+                                                   <h3>Due markes<br><strong class="ornage_color">Rental</strong></h3>
+                                                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i>
+                                                   </p>
+                                                   <div class="full text_align_center margin_top_30">
+                                                       <img src="icon/testimonial_qoute.png">
+                                                   </div>
+                                               </div>
+                                           </div>
+                                       </div>
+                                   </div>
+               
+                               </div>
+               
+                           </div>
+               
+                       </div>
+                   </div>
+                   <!-- end clients -->
+                   <!-- contact -->
+                   <div class="contact">
+                       <div class="container">
+                           <div class="row">
+                               <div class="col-md-12">
+                                   <div class="titlepage">
+                                       <h2>Contact us</h2>
+                                   </div>
+                                   <form class="main_form">
+                                       <div class="row">
+                                           <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                               <input class="form-control" placeholder="Your name" type="text" name="Your Name">
+                                           </div>
+                                           <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                               <input class="form-control" placeholder="Email" type="text" name="Email">
+                                           </div>
+                                           <div class=" col-md-12">
+                                               <input class="form-control" placeholder="Phone" type="text" name="Phone">
+                                           </div>
+                                           <div class="col-md-12">
+                                               <textarea class="textarea" placeholder="Message"></textarea>
+                                           </div>
+                                           <div class=" col-md-12">
+                                               <button class="send">Send</button>
+                                           </div>
+                                       </div>
+                                   </form>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <!-- end contact -->
+                   @include('layout.footer')
+                   <!-- Javascript files-->
+                   <script src={{asset("js/jquery.min.js")}}></script>
+                   <script src={{asset("js/popper.min.js")}}></script>
+                   <script src={{asset("js/bootstrap.bundle.min.js")}}></script>
+                   <script src={{asset("js/jquery-3.0.0.min.js")}}></script>
+                   <script src={{asset("js/plugin.js")}}></script>
+                   <!-- sidebar -->
+                   <script src={{asset("js/jquery.mCustomScrollbar.concat.min.js")}}></script>
+                   <script src={{asset("js/custom.js")}}></script>
+                   <!-- javascript -->
+                   <script src={{asset("js/owl.carousel.js")}}></script>
+                   <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+                   <script>
+                       $(document).ready(function() {
+                           $(".fancybox").fancybox({
+                               openEffect: "none",
+                               closeEffect: "none"
+                           });
+               
+                           $(".zoom").hover(function() {
+               
+                               $(this).addClass('transition');
+                           }, function() {
+               
+                               $(this).removeClass('transition');
+                           });
+                       });
+                   </script>
+               </body>
+</html>
